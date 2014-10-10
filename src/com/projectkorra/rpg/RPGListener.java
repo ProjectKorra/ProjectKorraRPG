b759b24393f7e07cafbe80a166fad5fc02a1e848
AlexTheCoder <groupmoderatorforever@gmail.com>
2014-10-10 16:03:56 -0400

    AvatarStateOnFinalBlow Patch #1
    Fixed the final blow being cancelled regardless of the cooldown status
    of the Avatar State resulting in a player who cannot die.

--- src/com/projectkorra/rpg/RPGListener.java
+++ src/com/projectkorra/rpg/RPGListener.java
@@ -5,6 +5,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 
+import com.projectkorra.ProjectKorra.BendingPlayer;
 import com.projectkorra.ProjectKorra.Methods;
 import com.projectkorra.ProjectKorra.Ability.AvatarState;
 
@@ -19,6 +20,10 @@ public class RPGListener implements Listener{
 		if(event.getEntity() instanceof Player) {
 			Player player = (Player) event.getEntity();
 			if(Methods.getBendingPlayer(player.getName()) != null) {
+				BendingPlayer bp = Methods.getBendingPlayer(player.getName());
+				
+				if(bp.isOnCooldown("AvatarState")) return;
+				
 				if(Methods.hasPermission(player, "AvatarState")) {
 					if(player.getHealth() - event.getDamage() <= 0) {
 						event.setCancelled(true);

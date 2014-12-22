ab407fdaacde7fba3214ee03222bd3838a9c0e13
MistPhizzle <mistphizzle@gmail.com>
2014-12-22 11:48:29 -0500

    Change how AvatarState onFinalBlow Works

--- src/com/projectkorra/rpg/RPGListener.java
+++ src/com/projectkorra/rpg/RPGListener.java
@@ -16,17 +16,14 @@ public class RPGListener implements Listener{
 	public void onAvatarDamaged(EntityDamageEvent event) {
 		if(event.isCancelled()) return;
 		
-		if(!ProjectKorraRPG.plugin.getConfig().getBoolean("Abilities.AvatarStateOnFinalBlow")) return;
-		
-		if(event.getEntity() instanceof Player) {
-			Player player = (Player) event.getEntity();
-			if(Methods.getBendingPlayer(player.getName()) != null) {
-				BendingPlayer bp = Methods.getBendingPlayer(player.getName());
-				
-				if(bp.isOnCooldown("AvatarState")) return;
-				
-				if(Methods.hasPermission(player, "AvatarState")) {
-					if(player.getHealth() - event.getDamage() <= 0) {
+		if(ProjectKorraRPG.plugin.getConfig().getBoolean("Abilities.AvatarStateOnFinalBlow")) {
+			if (event.getEntity() instanceof Player) {
+				Player player = (Player) event.getEntity();
+				if (Methods.getBendingPlayer(player.getName()) != null && (RPGMethods.isCurrentAvatar(player.getUniqueId()) || RPGMethods.hasBeenAvatar(player.getUniqueId()))) {
+					BendingPlayer bP = Methods.getBendingPlayer(player.getName());
+					
+					if (bP.isOnCooldown("AvatarState")) return;
+					if (player.getHealth() - event.getDamage() <= 0) {
 						event.setCancelled(true);
 						new AvatarState(player);
 					}
